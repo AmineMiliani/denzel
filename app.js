8a3f50860c050b9f350cd829bf12061dfec1f0fe
AmineMiliani <miliani.amine@orange.fr>
2019-03-05 16:03:00 +0100

    Api improved

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
+const imdb = require('./src/imdb');
 const CONNECTION_URL = "mongodb+srv://Amine:test@denzel-ya9cl.mongodb.net/database?retryWrites=true";
 const DATABASE_NAME = "database";
-
+const DENZEL_IMDB_ID = 'nm0000243';
+const actor = DENZEL_IMDB_ID
 var app = Express();
 
 app.use(BodyParser.json());
@@ -13,30 +15,50 @@ app.use(BodyParser.urlencoded({ extended: true }));
 var database, collection;
 
 app.listen(3000, () => {
-    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("filmography");
-        console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
+  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+      throw error;
+    }
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("filmography");
+    console.log("Connected to `" + DATABASE_NAME + "`!");
+  });
 });
 
 app.post("/movies", (request, response) => {
-    collection.insert(request.body, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result.result);
-    });
+  collection.insert(request.body, (error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result.result);
+  });
 });
 
 app.get("/movies", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+  collection.find({}).toArray((error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
+});
+
+app.post("/movies/populate", async (request,response) => {
+  const movies = await imdb(actor);
+  const str_movies = JSON.stringify(movies);
+  collection.insertMany(movies,(error,result) =>{
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result.result);
+  });
+});
+
+app.get("/movies/:id", (request, response) => {
+  collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
 });
